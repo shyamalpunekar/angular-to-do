d01e4cf436da41b6c97109f4c59c61466b564ac6
shy <shyamal.punekar@gmail.com>
2017-09-18 13:37:46 -0700

    Add 'display dynamic data' to ts file

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,11 +4,28 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class= "container">
-  <h1>My First Angular 2 App</h1>
+  <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+  <h3>{{currentFocus}}</h3>
+  <ul>
+    <li>{{firstTask.description}}</li>
+  </ul>
   </div>
   `
 })
 
 export class AppComponent {
+  currentFocus: String = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  firstTask = {
+    description: "Finish weekend Angular homework for Epicodus course"
+  }
+
+  export class Task{
+    
+  }
 
 }
